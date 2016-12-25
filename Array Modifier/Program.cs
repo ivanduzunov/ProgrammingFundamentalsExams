522bc9e6d407e1a241fc7525bcaeb03e821aa1af
ivan.d.uzunov <ivan.d.uzunov@gmail.com>
2016-12-25 23:29:26 +0200

    1

---
+++ Array Modifier/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<long> sequence = Console.ReadLine().Split().Select(long.Parse).ToList();
+            string command = Console.ReadLine();
+            while (!command.Equals("end"))
+            {
+                string[] splitted = command.Split(' ').ToArray();
+                string action = splitted[0];
+                switch (action)
+                {
+                    case "swap":
+                        string first = splitted[1]; string second = splitted[2]; Swap(sequence, first, second);break;
+                    case "multiply": string firstM = splitted[1]; string secondM = splitted[2]; Multiply(sequence, firstM, secondM); break;
+                    case "decrease": Decrease(sequence); break;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(", ", sequence));
+        }
+        static void Swap(List<long> sequence, string first, string second)
+        {
+            int firstIndex = int.Parse(first);
+            int secondIndex = int.Parse(second);
+            long firstNumber = sequence[secondIndex];
+            long secondNumber = sequence[firstIndex];
+            sequence.RemoveAt(firstIndex);
+            sequence.Insert(firstIndex, firstNumber);
+            sequence.RemoveAt(secondIndex);
+            sequence.Insert(secondIndex, secondNumber);          
+        }
+        static void Multiply(List<long> sequence, string first, string second)
+        {
+            int firstIndex = int.Parse(first);
+            int secondIndex = int.Parse(second);
+            long firstNumber = sequence[secondIndex];
+            long secondNumber = sequence[firstIndex];
+            sequence.RemoveAt(firstIndex);
+            sequence.Insert(firstIndex, firstNumber * secondNumber);
+        }
+        static void Decrease(List<long> sequence)
+        {
+            for (int i = 0; i < sequence.Count; i++)
+            {
+                long number = sequence[i];
+                sequence[i] = number - 1;
+            }
+        }
+
+    }
+}
---
+++ SweetDessert/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweetDessert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double amountOfCash = double.Parse(Console.ReadLine());
+            int guests = int.Parse(Console.ReadLine());
+            double bananasPrice = double.Parse(Console.ReadLine());
+            double eggSinglePrice = double.Parse(Console.ReadLine());
+            double berriesKiloPrice = double.Parse(Console.ReadLine());
+            double berriesPriceForOneSet = berriesKiloPrice * 0.2;
+
+            double neededSets = Math.Ceiling((double)guests / 6);
+            double neededCash = (neededSets * (2 * bananasPrice)) +
+                (neededSets * (4 * eggSinglePrice)) + (neededSets * berriesPriceForOneSet);
+
+            if (neededCash <= amountOfCash)
+            {
+                Console.WriteLine("Ivancho has enough money - it would cost {0:f2}lv.", neededCash);
+            }
+            else
+            {
+                Console.WriteLine("Ivancho will have to withdraw money - he will need {0:f2}lv more.", neededCash - amountOfCash);
+            }
+        }
+    }
+}
